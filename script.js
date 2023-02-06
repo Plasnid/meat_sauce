c3d4df0fa6443ac92b16771619fc6b177d776e3b
Plasnid <stuart@sackler.ca>
2023-02-06 10:10:03 -0500

    function, if statement and operator examples added.

--- script.js
+++ script.js
@@ -43,9 +43,75 @@ function vegWash(veggieList){
     }
 }
 // * carrots, tomato, spinach, basil, onions, garlic, celery
-let vegetables = ["carrots", "tomato", "spinach", "basil", "onions", "garlic", "celery", "eggplant"];
+let ingredients = [
+    {name: "carrots", amount: "2 blended"}, 
+    {name: "tomato", amount:"40 cubed"}, 
+    {name: "spinach", amount:"1 leaf"}, 
+    {name: "basil", amount:"3 clubbed"}, 
+    {name: "onions", amount:"4 roasted"}, 
+    {name: "garlic", amount:"80 cups"}
+];
+console.log(ingredients[3]["name"]);
+console.log(ingredients[3].amount);
+console.log(`you will need ${ingredients[3].amount} of ${ingredients[3].name}`)
 
-vegWash(vegetables);
+/**
+ * operators
+ */
+
+/**
+ * * = sets a value let bork="meetboooool";
+ * * ==  compares equality but not datatype  1=="1" would pass
+ * * === compares equality and datatype 1==="1" would fail
+ * * != not equal
+ * 
+ * * > greater than
+ * * < less than
+ * * >= greater than or equal to
+ * * <= less than or equal to
+ * * ! not
+ * * && and
+ */
+
+let queasey = false;
+let dietType = "cannibal";
+let hungry = true;
+
+function letsEat(dietType, hungerLimit){
+    let randoHunger = Math.random();
+    console.log(randoHunger);
+    if(randoHunger<hungerLimit){
+        console.log("I would like some foods");
+        if(dietType=="cannibal"){
+            console.log("Why don't you join me as dinner");
+        }
+    }else if(hungry==false && dietType=="cannibal"){
+        console.log("pleased to meat you!  I'll save you for later.")
+    }else{
+        console.log("I could definitely eat!");
+    }
+}
+letsEat("cannibal",0.7)
+
+
+/*let sharpThings = {
+    undersea: "shark mouth",
+    hospital: "scalpel",
+    australia: "everything",
+    bathroom: "straight razor",
+    dangerLevel: 110,
+    dangerSpread: function(spreader, person, spread){
+        console.log(`${person} you are spreading ${spread} with a dangerous ${spreader}`);
+    }
+}
+sharpThings.dangerSpread("acid","Danger Stu","vegemite");
+*/
+
+/*console.log(sharpThings.undersea);
+sharpThings["shmear depositor"] = "ninja star";
+console.log(sharpThings["shmear depositor"]);
+*/
+//vegWash(vegetables);
 
 
 // * cutting
